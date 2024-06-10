8d4c78786163fa2ca11fb2ea8be754351bbd4fe7
Denis <denver.1299@mail.ru>
2024-06-10 18:45:18 +0300

    start new project(2)

---
+++ src/masks.py
@@ -0,0 +1,13 @@
+def get_mask_account(number: str) -> str:
+    """Функция, маскирующая номер счета или карты"""
+    if str(number).isdigit() and len(number) == 16:
+        return f"{number[:4]} {number[4:6]}** **** {number[12:]}"
+    elif str(number).isdigit() and len(number) == 20:
+        return f"**{number[-4::]}"
+    else:
+        return "Введены некоректные данные"
+
+
+print(get_mask_account(number="3234123412341234"))
+print(get_mask_account(number="53125674431399887757"))
+print(get_mask_account(number="fdgswe4252312653467547"))
