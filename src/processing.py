be193ac841018bf4f90da1c39c10fbb9c381711d
Denis <denver.1299@mail.ru>
2024-06-16 14:31:11 +0300

    func filter by state fixes

--- src/processing.py
+++ src/processing.py
@@ -4,7 +4,7 @@ def filter_by_state(dictionary_list: list, state: str = 'EXECUTED') -> list:
     new_list_ = []
 
     for i in dictionary_list:
-        if i.get("state") != state:
+        if i.get("state") == state:
             new_list_.append(i)
     return new_list_
 
@@ -15,6 +15,14 @@ print(filter_by_state(
      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
 
 
-# def sort_by_date(dictionary_list: list, date: int) -> list:
+# def sort_by_date(dictionary_list: list, date: int = 'date') -> list:
 #     """ Функция сортировки операций по дате """
-#     for i in dictionary_list:
+#     # sorted_dictionary_list = sorted(dictionary_list, key = 'date', reverse = False)
+#
+#     return dictionary_list.sort(key='date', reverse=False)
+#
+# print(sort_by_date(
+#     [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+#      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+#      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+#      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
