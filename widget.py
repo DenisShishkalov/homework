8996b82241524c414288a8bfe43f51bfb6cc9bc8
Denis <denver.1299@mail.ru>
2024-06-16 12:38:22 +0300

    add file widget

--- widget.py
+++ widget.py
@@ -1,6 +1,3 @@
-from src.masks import get_mask_account
-
-
 def mask_account_card(number: str) -> str:
     """ Функция принимает номер карты или номер счета"""
     name_card = ''
@@ -15,12 +12,14 @@ def mask_account_card(number: str) -> str:
 
         return f"{name_card} {number[-16: -12]} {number[-11:-9]}** **** {number[-4:]}"
 
+
 print(mask_account_card(number="Счет 12246578657898764356"))
-print(mask_account_card(number= "Visa 1245123412341234"))
+print(mask_account_card(number="Visa 1245123412341234"))
 
 
 def get_data(data: str) -> str:
     """ Функция преобразования даты """
     return f"{data[8:10]}.{data[5:7]}.{data[0:4]}"
+
 
 print(get_data(data="2018-07-11T02:26:18.671407"))
