8ac02e2366bea47d91ffe51bd5cecc0bff8bf754
Denis <denver.1299@mail.ru>
2024-06-16 13:55:28 +0300

    upgrade pyprojekt toml

--- src/processing.py
+++ src/processing.py
@@ -13,3 +13,8 @@ print(filter_by_state(
      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
      {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
      {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+
+
+# def sort_by_date(dictionary_list: list, date: int) -> list:
+#     """ Функция сортировки операций по дате """
+#     for i in dictionary_list:
