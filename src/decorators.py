22a3edc5529af05040b5385ba5cc3337c1e7c041
Denis <denver.1299@mail.ru>
2024-06-28 20:13:22 +0300

    add file test decorators

--- src/decorators.py
+++ src/decorators.py
@@ -2,7 +2,7 @@ from functools import wraps
 from typing import Callable, Any
 
 
-def log(filename: str) -> Callable:
+def log(filename: str | None = None) -> Callable:
     def decorator(func: Any):
         @wraps(func)
         def inner(*args, **kwargs) -> Any:
@@ -11,12 +11,14 @@ def log(filename: str) -> Callable:
                     result = func(*args, **kwargs)
                     with open(filename, 'a', encoding='utf-8') as file:
                         file.write('\nmy_function ok')
+
                 else:
                     print("\nmy_function ok")
 
-             except Exception as f:
+             except Exception as e:
                 with open(filename, 'a', encoding='utf-8') as file:
-                    file.write(f'my_function error: {f} Inputs: {args}, {kwargs}')
+                    file.write(f'\nmy_function error: {e} Inputs: {args}, {kwargs}')
+                raise Exception(f'Ошибка: {e}')
              return result
         return inner
     return decorator
@@ -27,4 +29,5 @@ def my_function(x, y):
     """ Функция, складывающая 2 числа"""
     return x + y
 
-my_function(1, 2)
+print(my_function(1, 2))
+# print(my_function(['fdh'], 'gfh'))
