98c89726e272da961a07007b1e567df5f2acfae0
Denis <denver.1299@mail.ru>
2024-06-28 20:24:48 +0300

    done

--- src/decorators.py
+++ src/decorators.py
@@ -3,31 +3,34 @@ from typing import Callable, Any
 
 
 def log(filename: str | None = None) -> Callable:
-    def decorator(func: Any):
+    def decorator(func: Any) -> Callable:
         @wraps(func)
-        def inner(*args, **kwargs) -> Any:
-             try:
+        def inner(*args: Any, **kwargs: Any) -> Callable:
+            try:
                 if filename:
                     result = func(*args, **kwargs)
-                    with open(filename, 'a', encoding='utf-8') as file:
-                        file.write('\nmy_function ok')
+                    with open(filename, "a", encoding="utf-8") as file:
+                        file.write("\nmy_function ok")
 
                 else:
                     print("\nmy_function ok")
 
-             except Exception as e:
-                with open(filename, 'a', encoding='utf-8') as file:
-                    file.write(f'\nmy_function error: {e} Inputs: {args}, {kwargs}')
-                raise Exception(f'Ошибка: {e}')
-             return result
+            except Exception as e:
+                with open(filename, "a", encoding="utf-8") as file:
+                    file.write(f"\nmy_function error: {e} Inputs: {args}, {kwargs}")
+                raise Exception(f"Ошибка: {e}")
+            return result
+
         return inner
+
     return decorator
 
 
 @log(filename="mylog.txt")
 def my_function(x, y):
-    """ Функция, складывающая 2 числа"""
+    """Функция, складывающая 2 числа"""
     return x + y
+
 
 print(my_function(1, 2))
 # print(my_function(['fdh'], 'gfh'))
