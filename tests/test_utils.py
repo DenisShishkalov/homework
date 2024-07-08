6a2e6a5ac9ca7587e4c2a56a16a44a36ea3f7224
Denis <denver.1299@mail.ru>
2024-07-08 16:40:03 +0300

    fix test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,11 @@
+import json
 from unittest.mock import Mock
+
 from src.utils import get_info
 
 
 def test_get_info():
-    mock_file_json = Mock(return_value=r"C:\Users\Денис\PycharmProjects\01\data\operations.json")
+    mock_get_info = Mock(return_value=r"C:\Users\Денис\PycharmProjects\01\data\operations.json")
+    get_info = mock_get_info
+    get_info(file_json=r"C:\Users\Денис\PycharmProjects\01\data\operations.json") == r"C:\Users\Денис\PycharmProjects\01\data\operations.json"
+    mock_get_info.assert_called_once_with(file_json=r"C:\Users\Денис\PycharmProjects\01\data\operations.json")
