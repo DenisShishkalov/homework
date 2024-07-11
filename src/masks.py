c82da8b4712e9093561d614bc858af9606aa362f
Denis <denver.1299@mail.ru>
2024-07-11 16:08:01 +0300

    update README, create log_files

--- src/masks.py
+++ src/masks.py
@@ -1,14 +1,29 @@
+import logging
+
+logger = logging.getLogger('masks')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(r'C:\Users\Денис\PycharmProjects\01\logs\masks.log', 'w')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
+
+
 def get_mask_account(number: str) -> str:
     """Функция, маскирующая номер счета или карты"""
+    try:
+        logger.info('Проверяем корректность полученных данных и маскируем их')
+        if str(number).isdigit() and len(number) == 16:
+            return f"{number[:4]} {number[4:6]}** **** {number[12:]}"
+        elif str(number).isdigit() and len(number) == 20:
+            return f"**{number[-4::]}"
+        else:
+            logger.info("Выводим сообщение, что данные некорректны")
+            return 'Введены некорректные данные'
+    except Exception as ex:
+        logger.error(f"Произошла ошибка: {ex}")
 
-    if str(number).isdigit() and len(number) == 16:
-        return f"{number[:4]} {number[4:6]}** **** {number[12:]}"
-    elif str(number).isdigit() and len(number) == 20:
-        return f"**{number[-4::]}"
-    else:
-        return 'Введены некорректные данные'
 
 
-print(get_mask_account(number="3234123412341234"))
-print(get_mask_account(number="53125674431399887757"))
+# print(get_mask_account(number="3234123412341234"))
+# print(get_mask_account(number="53125674431399887757"))
 print(get_mask_account(number="fdgswe4252312653467547"))
