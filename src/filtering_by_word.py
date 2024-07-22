78f3914e22a4675c7718f404ac0663934a9d139a
Denis <denver.1299@mail.ru>
2024-07-22 11:07:06 +0300

    fix modul grouping and filtering by word

--- src/filtering_by_word.py
+++ src/filtering_by_word.py
@@ -2,28 +2,21 @@ import json
 import re
 
 
-def filtered_operations(list_dict: str, search: str) -> list[dict]:
+def filtered_operations(list_dict: list[dict], search: str) -> list[dict]:
     """
     Функция, которая будет принимать список словарей с данными о банковских операциях и строку поиска,
     а возвращать список словарей, у которых в описании есть данная строка.
     """
-    with open(list_dict, encoding='utf-8') as f:
-        data_ = json.load(f)
-        operations_ = []
-        for i in data_:
-            if i.get('state'):
-                operations_.append(i)
-        return [trans for trans in operations_ if re.search(search, trans['state'])]
+    operations_ = []
+    for i in list_dict:
+        if i.get('description'):
+            operations_.append(i)
+    return [trans for trans in operations_ if re.search(search, trans['description'])]
 
 
 if __name__ == '__main__':
-    # path = r'C:\Users\Денис\PycharmProjects\01\data\operations.json'
-    # with open(path, encoding='utf-8') as f:
-    #     data_ = json.load(f)
-
-    filtered_operations(r'C:\Users\Денис\PycharmProjects\01\data\operations.json', 'CANCELED')
-    filtered_operations(r'C:\Users\Денис\PycharmProjects\01\data\operations.json', 'PENDING')
-    filtered_operations(r'C:\Users\Денис\PycharmProjects\01\data\operations.json', 'EXECUTED')
-    # filtered_operations(data_, 'CANCELED')
-    # filtered_operations(data_, 'PENDING')
-    # filtered_operations(data_, 'EXECUTED')
+    path = r'..\data\operations.json'
+    with open(path, encoding='utf-8') as f:
+        data_ = json.load(f)
+        search_string = 'Перевод организации'
+    filtered_operations(data_, search_string)
--- src/grouping.py
+++ src/grouping.py
@@ -1,36 +1,26 @@
 import json
-import re
 from collections import Counter
 
 
-def grouping_operations(transactions: str, category: str) -> dict:
+def grouping_operations(transactions: list[dict], category: list) -> dict:
     """
     Функция, которая будет принимать список словарей с данными о банковских операциях и список категорий операций,
     а возвращать словарь, в котором ключи — это названия категорий, а значения — это количество операций в каждой
     категории.
     """
-    with open(transactions, encoding='utf-8') as f:
-        sentence = json.load(f)
-        operations_ = []
-        for i in sentence:
-            if i.get('description'):
-                operations_.append(i)
-        result = [category for trans in operations_ if re.findall(category, trans['description'])]
-
-        c = dict(Counter(result))
-        return c
-
-        # trans = []
-        # for i in transaction_list:
-        #     if i.get('descriptions'):
-        #         trans.append(i)
-        #     return trans
-    # return [op for op in trans if re.search(category, op['descriptions'])]
+    operations_ = []
+    for i in transactions:
+        if i.get('description'):
+            operations_.append(i)
+            result = [trans['description'] for trans in operations_ if trans['description'] in category]
+            count_ = Counter(result)
+            count_dict = dict(count_)
+    return count_dict
 
 
 if __name__ == '__main__':
-    # list_category = ['Перевод организации, Открытие вклада, Перевод с карты на карту, Перевод с карты на счет']
-    print(grouping_operations(r'C:\Users\Денис\PycharmProjects\01\data\operations.json', 'Перевод организации'))
-    print(grouping_operations(r'C:\Users\Денис\PycharmProjects\01\data\operations.json', 'Перевод с карты на карту'))
-    print(grouping_operations(r'C:\Users\Денис\PycharmProjects\01\data\operations.json', "Перевод с карты на счет"))
-    print(grouping_operations(r'C:\Users\Денис\PycharmProjects\01\data\operations.json', "Открытие вклада"))
+    path = r'..\data\operations.json'
+    with open(path, encoding='utf-8') as f:
+        sentence = json.load(f)
+    list_category = ['Перевод организации', 'Открытие вклада', 'Перевод с карты на карту', 'Перевод с карты на счет']
+    grouping_operations(sentence, list_category)
